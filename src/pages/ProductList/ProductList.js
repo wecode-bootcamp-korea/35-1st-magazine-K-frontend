5a7f4af5b6d41c8350d7e9b0bac72af4360076b4
foafls <>
2022-07-18 22:44:28 +0900

    Add: ProductList.js 추가 기본적인 레이아웃 구성 중

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import './ProductList.scss';
 
 const ProductList = () => {
-  return <div />;
+  return (
+    <div className="productlist">
+      <div className="menuTapContainer">
+        <div className="magazineKTap">Magazine K</div>
+        <div className="fashionTap">Fashion</div>
+        <div className="beautyTap">Beauty</div>
+        <div className="designLifestyleTap">Design Lifestyle</div>
+      </div>
+      <div className="productlistContainer">
+        <div className="productWrapper">
+          <img
+            className="product1-1"
+            src="/images/product1-1.jpg"
+            alt="product1-1"
+          />
+          <img
+            className="product1-2"
+            src="/images/product1-2.jpg"
+            alt="product1-2"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductList;
