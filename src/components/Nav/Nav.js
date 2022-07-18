42621f61adc699fc785412f4a5aa940a8368c8c9
foafls <>
2022-07-18 22:01:29 +0900

    Add: Nav.js 기본적인 nav바 구성 완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Nav.scss';
 
 const Nav = () => {
-  return <div />;
+  const navigate = useNavigate();
+
+  return (
+    <nav className="nav">
+      <div className="firstMenuContainer">
+        <img
+          className="magazinIcon"
+          onClick={() => {
+            navigate('/');
+          }}
+          src="/images/k.png"
+          alt="kIcon"
+        />
+        <div
+          className="magazineMenu"
+          onClick={() => {
+            navigate('/ProductList');
+          }}
+        >
+          Magazine
+        </div>
+      </div>
+      <div className="secondMenuContainer">
+        <div className="searchMenu">Search</div>
+        <div
+          className="loginMenu"
+          onClick={() => {
+            navigate('/Login');
+          }}
+        >
+          Login
+        </div>
+        <div className="cartMenu">Cart[0]</div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
