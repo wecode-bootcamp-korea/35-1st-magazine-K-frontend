f85dd7d16e53dfa29ed107b47882b4ae23cf9ce8
길현민 <jxm9331@gmail.com>
2022-07-19 15:29:35 +0900

    Login&SignUp Css

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import './Login.scss';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div className="LoginContainer">
-      <div>
-        <input />
-        {/* 아이디는 영문소문자 또는 숫자 4~16자로 입력 */}
-      </div>
-      <div>
-        <input />
-        {/* 대소문자/숫자/특수문자 중 3가지 이상 조합, 8자~16자 입력가능특수문자 ~`!@#$%^()_-={}[]|;:<>,.?/ */}
-      </div>
-      <div>
-        <button>로그인</button>
-      </div>
-      <div>
-        <button>주문조회</button>
-        <button>아이디찾기</button>
-        <button>비밀번호찾기</button>
-        <button>회원가입</button>
+    <div className="loginPageWrapper">
+      <div className="loginBox">
+        <div>
+          <input />
+          {/* 아이디는 영문소문자 또는 숫자 4~16자로 입력 */}
+        </div>
+        <div>
+          <input type="password" />
+          {/* 대소문자/숫자/특수문자 중 3가지 이상 조합, 8자~16자 입력가능특수문자 ~`!@#$%^()_-={}[]|;:<>,.?/ */}
+        </div>
+        <div>
+          <button>로그인</button>
+        </div>
+        <div>
+          <button>주문조회</button>
+          <button>아이디찾기</button>
+          <button>비밀번호찾기</button>
+          <Link to="/SignUp">
+            <button>회원가입</button>
+          </Link>
+          {/* <Link to="/main-hyeonmin">Westagram</Link> */}
+        </div>
       </div>
     </div>
   );
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import './SignUp.scss';
 
 const SignUp = () => {
-  return <div />;
+  return (
+    <div className="signUPageWrapper">
+      <div className="signUPBox">
+        <p className="signUpItem">아이디*</p>
+        <input className="signUpItem" />
+        <p className="signUpItem">비밀번호*</p>
+        <input className="signUpItem" />
+        <p className="signUpItem">비밀번호 확인*</p>
+        <input className="signUpItem" />
+        <p className="signUpItem">이름*</p>
+        <input className="signUpItem" />
+        <p className="signUpItem">휴대전화</p>
+        <input type="tel" className="signUpItem" />
+        <p className="signUpItem">이메일*</p>
+        <input type="email" className="signUpItem" />
+        <p className="serviceAgree">서비스 이용 동의</p>
+        <div className="inputWrapper agreeAll">
+          <label>
+            <input type="checkbox" />
+            이용약관에 모두 동의합니다.(선택 정보 포함)
+          </label>
+        </div>
+        <div className="inputWrapper">
+          <label>
+            <input type="checkbox" />
+            [필수] <span>이용약관</span>에 동의합니다.
+          </label>
+        </div>
+        <div className="inputWrapper">
+          <label>
+            <input type="checkbox" />
+            [필수] <span>개인정보 수집 및 이용</span>에 동의합니다.
+          </label>
+        </div>
+        <button>가입하기</button>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
