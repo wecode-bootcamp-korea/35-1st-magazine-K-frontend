c080ffbfab328d406cd43f23d46ef2952e8e076d
wjddms4107 <78889402+wjddms4107@users.noreply.github.com>
2022-07-19 21:17:11 +0900

    Add : main slide 좌우 클릭시,  3초마다 이미지 자동변환 기능 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,70 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/alt-text */
+import React, { useState, useEffect } from 'react';
+import './Main.scss';
 
-const Main = () => {
-  return <div />;
-};
+function Main() {
+  const [slide, setSlide] = useState(1);
 
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setSlide(slide + 1);
+  //   }, 3000);
+
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, [slide]);
+
+  // if (slide > 3) {
+  //   setSlide(1);
+  // }
+
+  function prevSlide() {
+    setSlide(slide - 1);
+  }
+
+  const nextSlide = () => {
+    setSlide(slide + 1);
+  };
+
+  const firstSlide = slide === 1;
+  const lastSlide = slide === 3;
+
+  return (
+    <div className="slideContainer">
+      <div className="slideImg">
+        <img src={`/images/main/mainSlide${slide}.jpg`} alt="mainSlide1" />
+        <button onClick={prevSlide} className="numLeft" disabled={firstSlide}>
+          prev
+        </button>
+        <button onClick={nextSlide} className="numRight" disabled={lastSlide}>
+          next
+        </button>
+        <div className="num">
+          <h1 className="prevNum">{slide}</h1>
+          <h1 className="nextNum">3</h1>
+        </div>
+        <div className="slideInfo">
+          <span>MAGAZINE B</span>
+          <span>ISSUE NO.90</span>
+          <div>LEMAIRE</div>
+          <p>
+            1991년 프랑스 파리에서 크리스토프 르메르가 설립한 르메르는
+            <br />
+            일상복이야말로 옷에 내재된 가능성을 유연하게 포현하는 평식임을
+            보여줍니다.
+          </p>
+          <a>SHOP NOW</a>
+        </div>
+      </div>
+      {/* <div className="slideImg">
+          <img src="/images/main/mainSlide2.jpg" alt="mainSlide2" />
+        </div>
+        <div className="slideImg">
+          <img src="/images/main/mainSlide3.jpg" alt="mainSlide3" />
+        </div> */}
+    </div>
+  );
+}
 export default Main;
