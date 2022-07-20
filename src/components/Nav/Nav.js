fe7947fb6840fa9b70f2a4e285e11c1871589bd2
foafls <>
2022-07-20 20:10:56 +0900

    Modify: nav바 로그인시 메뉴탭 바뀌는 기능 구현 중

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import './Nav.scss';
+
+const Login = ({ isLogin }) => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className="loginMenu"
+      onClick={() => {
+        isLogin ? navigate('/Login') : navigate('/MyPage');
+      }}
+    >
+      {isLogin ? 'Login' : 'My Page'}
+    </div>
+  );
+};
 
 const Nav = () => {
   const navigate = useNavigate();
+  const [isLogin] = useState(false);
 
   return (
     <nav className="nav">
@@ -27,14 +43,7 @@ const Nav = () => {
       </div>
       <div className="secondMenuContainer">
         <div className="searchMenu">Search</div>
-        <div
-          className="loginMenu"
-          onClick={() => {
-            navigate('/Login');
-          }}
-        >
-          Login
-        </div>
+        <Login isLogin={isLogin} />
         <div className="cartMenu">Cart[0]</div>
       </div>
     </nav>
