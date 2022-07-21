dfe16fcb2e3520a7caaae27139a9cddfa634807d
foafls <>
2022-07-21 09:50:14 +0900

    Modify: 네브바 css 보수중

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,16 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import './Nav.scss';
 
-const LoginTap = ({ isLogin }) => {
-  const navigate = useNavigate();
+const LoginTap = ({ token, movePage }) => {
   return (
     <div
       className="Menu"
       onClick={() => {
-        isLogin ? navigate('/MyPage') : navigate('/Login');
+        token ? movePage('/MyPage') : movePage('/Login');
       }}
     >
-      {isLogin ? 'My Page' : 'Login'}
+      {token ? 'My Page' : 'Login'}
     </div>
   );
 };
@@ -20,6 +19,10 @@ const LoginTap = ({ isLogin }) => {
 const Nav = () => {
   const navigate = useNavigate();
   const [isLogin] = useState(false);
+  let token = localStorage.getItem('token');
+  const movePage = targetPg => {
+    navigate(`${targetPg}`);
+  };
 
   return (
     <nav className="nav">
@@ -27,7 +30,7 @@ const Nav = () => {
         <img
           className="magazinIcon"
           onClick={() => {
-            navigate('/');
+            movePage('/');
           }}
           src="/images/k.png"
           alt="kIcon"
@@ -35,7 +38,7 @@ const Nav = () => {
         <div
           className="magazineMenu"
           onClick={() => {
-            navigate('/ProductList');
+            movePage('/');
           }}
         >
           Magazine
@@ -43,7 +46,7 @@ const Nav = () => {
       </div>
       <div className="secondMenuContainer">
         <div className="Menu">Search</div>
-        <LoginTap isLogin={isLogin} />
+        <LoginTap token={token} movePage={movePage} />
         <div className="Menu">Cart[0]</div>
       </div>
     </nav>
