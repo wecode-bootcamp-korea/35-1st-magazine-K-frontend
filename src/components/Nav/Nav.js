2bd9e94f5293ab8f5f1462d2005d0ab2c5dfae7b
foafls <>
2022-07-21 17:15:10 +0900

    Modify: Nav.js 2차 리뷰 반영

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,24 +2,11 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Nav.scss';
 
-const LoginTap = ({ token, movePage }) => {
-  return (
-    <div
-      className="Menu"
-      onClick={() => {
-        token ? movePage('/MyPage') : movePage('/Login');
-      }}
-    >
-      {token ? 'My Page' : 'Login'}
-    </div>
-  );
-};
-
 const Nav = () => {
   const navigate = useNavigate();
   let token = localStorage.getItem('token');
   const movePage = targetPg => {
-    navigate(`${targetPg}`);
+    navigate(targetPg);
   };
 
   return (
@@ -44,7 +31,14 @@ const Nav = () => {
       </div>
       <div className="secondMenuContainer">
         <div className="Menu">Search</div>
-        <LoginTap token={token} movePage={movePage} />
+        <div
+          className="menus"
+          onClick={() => {
+            token ? movePage('/MyPage') : movePage('/Login');
+          }}
+        >
+          {token ? 'My Page' : 'Login'}
+        </div>{' '}
         <div className="Menu">Cart[0]</div>
       </div>
     </nav>
