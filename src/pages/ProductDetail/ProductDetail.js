c0ec058ae1033029c8515b21ab343a61c6e288c5
wjddms4107 <78889402+wjddms4107@users.noreply.github.com>
2022-07-22 14:04:35 +0900

    Add : query parameter 세팅 완료

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,25 +1,24 @@
 /* eslint-disable react/jsx-key */
 import React, { useState, useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import Detail from './Detail/Detail';
 
 const ProductDetail = () => {
   const [prdDetailData, setPrdDetailData] = useState([]);
-  const [searchParams] = useSearchParams();
-  const cate_no = searchParams.get('cate_no');
-  const issue_number = searchParams.get('issue_number');
+  const location = useLocation();
+  // const cate_no = searchParams.get('cate_no');
+  // const issue_number = searchParams.get('issue_number');
+  // console.log(location.search);
 
   useEffect(() => {
-    fetch(
-      `http://10.58.4.28:8000/products/detail?issue=${parseInt(
-        issue_number
-      )}&category=${parseInt(cate_no)}`
-    )
+    fetch(`http://10.58.4.28:8000/products/detail${location.search}`)
       .then(res => res.json())
       .then(data => {
+        // console.log(data);
         setPrdDetailData(data.result);
       });
-  }, [cate_no, issue_number]);
+  }, []);
+
   return prdDetailData.map(prdDetailData => {
     return (
       <Detail key={prdDetailData.issue_number} prdDetailData={prdDetailData} />
