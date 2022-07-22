9ea59ac01a51de492d5be7840a9535b24797f0d6
foafls <>
2022-07-22 15:08:06 +0900

    Modify: 검색 모달 창 레이아웃 구현완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,47 +1,66 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import './Nav.scss';
 
 const Nav = () => {
   const navigate = useNavigate();
-  let token = localStorage.getItem('token');
+  const token = localStorage.getItem('token');
   const movePage = targetPg => {
     navigate(targetPg);
   };
+  const [isClicked, setIsClicked] = useState(false);
+  const toggleSearch = () => {
+    isClicked ? setIsClicked(false) : setIsClicked(true);
+  };
 
   return (
-    <nav className="nav">
-      <div className="firstMenuContainer">
-        <img
-          className="magazinIcon"
-          onClick={() => {
-            movePage('/');
-          }}
-          src="/images/k.png"
-          alt="kIcon"
-        />
-        <div
-          className="magazineMenu"
-          onClick={() => {
-            movePage('/');
-          }}
-        >
-          Magazine
+    <div className="nav">
+      <nav className="navigationContainer">
+        <div className="firstMenuContainer">
+          <img
+            className="magazinIcon"
+            onClick={() => {
+              movePage('/');
+            }}
+            src="/images/k.png"
+            alt="kIcon"
+          />
+          <div
+            className="magazineMenu"
+            onClick={() => {
+              movePage('/');
+            }}
+          >
+            Magazine
+          </div>
         </div>
-      </div>
-      <div className="secondMenuContainer">
-        <div className="Menu">Search</div>
-        <div
-          className="menus"
-          onClick={() => {
-            token ? movePage('/MyPage') : movePage('/Login');
-          }}
-        >
-          {token ? 'My Page' : 'Login'}
-        </div>{' '}
-        <div className="Menu">Cart[0]</div>
-      </div>
-    </nav>
+        <div className="secondMenuContainer">
+          <div className="menus" onClick={toggleSearch}>
+            Search
+          </div>
+          <div
+            className="menus"
+            onClick={() => {
+              token ? movePage('/MyPage') : movePage('/Login');
+            }}
+          >
+            {token ? 'My Page' : 'Login'}
+          </div>{' '}
+          <div className="menus">Cart[0]</div>
+        </div>
+      </nav>
+      <form className="searchModalContainer" onsubmit={() => {}}>
+        {isClicked && (
+          <input
+            className="searchBar"
+            type="text"
+            placeholder="Search Brand, City, Food..."
+            autoFocus
+          />
+        )}
+      </form>
+    </div>
   );
 };
 
