afbc7cbcb487ebe2a74bb78aaa2c545005b1a412
wjddms4107 <78889402+wjddms4107@users.noreply.github.com>
2022-07-24 15:04:51 +0900

    상세페이지로 브랜치 이동

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -2,13 +2,51 @@ import React, { useState, useEffect } from 'react';
 import './Cart.scss';
 
 const Cart = () => {
-  const [orderQuantity, setOrderQuantity] = useState(0);
   const [cartData, setCartData] = useState([]);
+  const [orderQuantity, setOrderQuantity] = useState(1);
   const [cartModal, setCartModal] = useState(true);
+  const [total, setTotal] = useState(0);
+
+  // console.log(cartData);
+
+  const totalPrice = cartData[0]?.price.toString().slice(0, 2) * orderQuantity;
+  // console.log(totalPrice);
+
+  function changeTotal() {
+    setTotal(totalPrice);
+  }
+
+  function getCartDataPrice(cartData) {
+    let cartDataPrice = [];
+    for (let i = 0; i < cartData.length; i++) {
+      cartDataPrice.push(
+        cartData[i].price.toString().slice(0, 2) * orderQuantity
+      );
+    }
+    return cartDataPrice;
+  }
+  const priceThousand = getCartDataPrice(cartData);
+  console.log(priceThousand && priceThousand?.reduce((a, b) => a + b));
+
+  // function totalPrice() {
+
+  // }
 
   const closeCart = () => {
     setCartModal(false);
   };
+
+  function minusOrderQuantity() {
+    if (orderQuantity < 2) {
+      setOrderQuantity(1);
+    } else {
+      setOrderQuantity(orderQuantity => orderQuantity - 1);
+    }
+  }
+
+  function plusOrderQuantity() {
+    setOrderQuantity(orderQuantity => orderQuantity + 1);
+  }
 
   useEffect(() => {
     fetch('data/cartData.json')
@@ -22,50 +60,72 @@ const Cart = () => {
     <div
       className="cartModal"
       style={cartModal ? { visibility: 'visible' } : { visibility: 'hidden' }}
-      onClick={closeCart}
     >
       <div className="cartNav">
         <span>Cart[{orderQuantity}]</span>
-        <span>Close</span>
+        <span onClick={closeCart}>Close</span>
       </div>
       <div className="cartMain">
         {/* <div className="empty">
             <p>장바구니가 비어 있습니다.</p>
           </div> */}
 
-        {/* 여기 map 돌리면 됨 */}
         {cartData.map(cartData => {
-          return <SelectedPrd key={cartData.id} cartData={cartData} />;
+          return (
+            <SelectedPrd
+              key={cartData.id}
+              cartData={cartData}
+              minusOrderQuantity={minusOrderQuantity}
+              plusOrderQuantity={plusOrderQuantity}
+              changeTotal={changeTotal}
+            />
+          );
         })}
       </div>
       <div className="cartFooter">
         <span>
-          ₩{orderQuantity === 0 ? 15 : 15 * orderQuantity},000 VIEW ALL
+          ₩{total}
+          ,000 VIEW ALL
         </span>
       </div>
     </div>
   );
 };
-function SelectedPrd({ cartData }) {
-  const { cart_img_url, id, price, title } = cartData;
+function SelectedPrd({
+  cartData,
+  minusOrderQuantity,
+  plusOrderQuantity,
+  changeTotal,
+}) {
+  const [orderQuantity, setOrderQuantity] = useState(1);
+  const totalPrice = cartData.price * orderQuantity;
+  // console.log(totalPrice);
+  // console.log(cartData);
 
-  const [orderQuantity, setOrderQuantity] = useState(0);
+  const plus = () => {
+    setOrderQuantity(prev => prev + 1);
+    plusOrderQuantity();
 
-  const [selected, setSelected] = useState(true);
+    const totalPrice = cartData.price * orderQuantity;
+    changeTotal(cartData.price * orderQuantity);
+    // console.log(totalPrice);
+  };
 
-  const priceThousand = price.toString().slice(0, 2);
-
-  function minusOrderQuantity() {
+  const minus = e => {
+    // console.log(e.target);
     if (orderQuantity < 2) {
       setOrderQuantity(1);
     } else {
-      setOrderQuantity(orderQuantity - 1);
+      setOrderQuantity(prev => prev - 1);
     }
-  }
+    minusOrderQuantity();
+    changeTotal(totalPrice);
+  };
 
-  function plusOrderQuantity() {
-    setOrderQuantity(orderQuantity + 1);
-  }
+  const [selected, setSelected] = useState(true);
+
+  const priceThousand = cartData.price.toString().slice(0, 2);
+  // console.log(priceThousand);
 
   function deleteProduct() {
     alert('선택하신 상품을 삭제하시겠습니까?');
@@ -74,24 +134,24 @@ function SelectedPrd({ cartData }) {
 
   return (
     <div
-      key={id}
+      key={cartData.id}
       className="selected"
       style={selected ? { display: 'flex' } : { display: 'none' }}
     >
       <div className="productInfo">
-        <img src={cart_img_url} alt="selected_tiny" />
+        <img src={cartData.cart_img_url} alt="selected_tiny" />
         <div className="description">
-          <div>{title}</div>
+          <div>{cartData.title}</div>
           <div>₩{priceThousand},000</div>
           <div className="count">
             <img
-              onClick={minusOrderQuantity}
+              onClick={minus}
               src="https://magazine-b.co.kr/web/baton/images/icon/icon_product_minus_white.png"
               alt="minus"
             />
             <span>{orderQuantity}</span>
             <img
-              onClick={plusOrderQuantity}
+              onClick={plus}
               src="https://magazine-b.co.kr/web/baton/images/icon/icon_product_plus_white.png"
               alt="plus"
             />
