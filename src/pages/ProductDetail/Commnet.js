13da74c8943cde63ee39bb0d576e7e77d9eac11e
wjddms4107 <78889402+wjddms4107@users.noreply.github.com>
2022-07-26 00:30:56 +0900

    Add: 댓글 기능 구현중

--- src/pages/ProductDetail/Commnet.js
+++ src/pages/ProductDetail/Commnet.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Comment.scss';
 
-function Comment() {
+function Comment({ comment, selected }) {
+  console.log(comment);
   return (
     <>
       <div className="commentBox">
         <div className="commentItem">
-          <div className="comment">항상 쓰던 제품이에요! 너무 좋네요.</div>
+          <div className="comment">{comment}</div>
           <div className="userInfo">
-            <div className="star">별별별별별</div>
+            <div className="star">{selected}</div>
             <p className="userId">유저ID</p>
           </div>
         </div>
--- src/pages/ProductDetail/Review.js
+++ src/pages/ProductDetail/Review.js
@@ -1,29 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Review.scss';
 import Comment from './Commnet';
 
 function Review() {
-  const changeReview = () => {
-    console.log('a');
+  const [selected, setSelected] = useState('');
+  const [selectedIndex, setSelectedIndex] = useState();
+  const [value, setValue] = useState('');
+  const [comments, setComments] = useState([]);
+
+  const getTextareaValue = e => {
+    setValue(e.target.value);
   };
+
+  const addComment = e => {
+    e.preventDefault();
+    const copyComments = [...comments];
+    if (value.length < 6) {
+      alert('리뷰를 5글자 이상 입력해주세요.');
+    }
+    copyComments.push(value);
+    setComments(copyComments);
+    setValue('');
+  };
+
+  const changeSelect = e => {
+    setSelectedIndex(e.target.value);
+    setSelected(e.target.options[e.target.selectedIndex].text);
+  };
+
   return (
     <div className="reviewContainer">
       <div className="reviewTitle">
         <div className="review">REVIEW</div>
         <hr />
       </div>
-      <form className="reviewForm">
+      <form className="reviewForm" onSubmit={addComment}>
         <div className="textarea">
           <textarea
             id="reviewTextarea"
             name="reviewTextarea"
             rows="5"
             placeholder="리뷰를 작성해주세요."
+            onChange={getTextareaValue}
           />
         </div>
         <div className="click">
           <span className="selectBox">
-            <select onChange={changeReview} className="reviewSelect">
+            <select
+              onChange={changeSelect}
+              className="reviewSelect"
+              value={selectedIndex}
+            >
               <option value="1">⭑</option>
               <option value="2">⭑⭑</option>
               <option value="3">⭑⭑⭑</option>
@@ -38,7 +65,15 @@ function Review() {
         <hr />
       </div>
       <div className="commentBox">
-        <Comment />
+        {comments.map((comment, i) => {
+          return (
+            <Comment
+              key={comment + i + 1}
+              comment={comment}
+              selected={selected}
+            />
+          );
+        })}
       </div>
     </div>
   );
